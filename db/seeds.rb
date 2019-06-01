4c389c5e737a6f4182d6f2155f1c0f802cad819a
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-06-01 19:21:05 +0100

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,29 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
+
+
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-    praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi
-    sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
-    mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et
-    expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
-    nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
-    assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut o
-    fficiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-    repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur
-    a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
-    aut perferendis doloribus asperiores repellat."
+      body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+      praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi
+      sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
+      mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et
+      expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
+      nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
+      assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut o
+      fficiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
+      repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur
+      a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
+      aut perferendis doloribus asperiores repellat.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -26,10 +38,25 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title #{portfolio_item}",
-    subtitle: "My great Service" ,
+    subtitle: "Ruby on Rails" ,
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam,
+    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+    vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+    aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+    ratione voluptatem sequi nesciunt.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title #{portfolio_item}",
+    subtitle: "Angular" ,
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
     accusantium doloremque laudantium, totam rem aperiam,
     eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
