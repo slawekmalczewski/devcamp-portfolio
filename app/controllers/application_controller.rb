0ab126bf12361b0123922cad477bfe8cee722ea5
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-06-23 21:43:00 +0100

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
-
-  before_action :set_source
-
-  def set_source
-    # in session I create variable SOURCE and assigned to it a query string
-    # that would appear after character "q" e.g. www.portfolio/4?q=twitter
-    session[:source] = params[:q] if params[:q]
-  end
-
+  include SetSource
 end
