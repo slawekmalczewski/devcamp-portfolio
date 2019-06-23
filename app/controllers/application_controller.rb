5fa49666aa3512839bc4db9ec38164d4c3e40279
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-06-23 21:31:05 +0100

    Added session tracker for dynamic source link ver 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :set_source
 
   def set_source
+    # in session I create variable SOURCE and assigned to it a query string
+    # that would appear after character "q" e.g. www.portfolio/4?q=twitter
     session[:source] = params[:q] if params[:q]
   end
 
