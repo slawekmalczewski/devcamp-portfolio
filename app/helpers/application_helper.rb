2ed7edc1b2a899890f38e1413073707723984d2b
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-08-05 14:15:52 +0100

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,4 +61,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Slawek Malczewski Portfolio", sticky: false, time: 3000)
+  end
+
+
 end
