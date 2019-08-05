f968953ee2ec101f4af2925027c4849aef804071
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-08-05 22:44:36 +0100

    Implemented Twitter call on tech new page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,16 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
-    @skills = Skill.all 
+    @skills = Skill.all
   end
 
   def about
   end
 
   def contact
+  end
+
+  def tech_news
+    @tweets = SocialTool.twitter_search
   end
 end
