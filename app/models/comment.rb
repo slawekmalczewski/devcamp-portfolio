dd7c1478bf20e14b21745b5b0ec8318390c6caaa
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-08-10 21:43:16 +0100

    Implemented jquery functions for action cable comment feature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-validates :content, pressence: true, length: { minimum: 5, maximum: 1000}
+validates :content, presence: true, length: { minimum: 5, maximum: 1000}
 
 end
